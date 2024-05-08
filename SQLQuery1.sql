3dba0f5b9b03eaa2d4053c28f2af6eeb6f85e9fd
Bogdan Jakovlev <bogdan.jakovlev11@gmail.com>
2024-05-08 13:42:00 +0300

    loob protseduuri, mis loeb kokku töötajate arvu.

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -329,7 +329,7 @@ declare @TotalCount int
 exec spGetEmployeeCountByGender @EmployeeCount = @TotalCount out, @Gender = 'Male'
 print @TotalCount
 
----?
+--- loob protseduuri, mis loeb kokku töötajate arvu.
 create proc spTotalCount2
 @TotalCount int output
 as begin
