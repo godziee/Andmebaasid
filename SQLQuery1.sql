2a80b37c5e39fea6445874a538fb5f3906abb5b2
Bogdan Jakovlev <bogdan.jakovlev11@gmail.com>
2024-05-08 13:37:11 +0300

    vigade parandamine protseduuriga seonduv

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -289,12 +289,14 @@ spGetEmployees
 exec spGetEmployees
 execute spGetEmployees
 
---- 
-create proc spGetEmployeesByGenderAndDepartment
-@Gender nvarchar(20),
-@DepartmentId int
-as begin
-	select FirstName, Gender, DepartmentId from Employees where Gender = @Gender
+--- vigade parandamine protseduuriga seonduv
+create procedure spGetEmployeesByGenderAndDepartment
+	@Gender nvarchar(20),
+	@DepartmentId int
+as 
+begin
+	select FirstName, Gender, DepartmentId from Employees
+	where Gender = @Gender
 	and DepartmentId = @DepartmentId
 end
 
