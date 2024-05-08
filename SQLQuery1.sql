66852282edab4b27fbe956b56c74b61cbb733ccd
Bogdan Jakovlev <bogdan.jakovlev11@gmail.com>
2024-05-08 13:09:42 +0300

    sorteerimine vanuse järgi

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -147,7 +147,7 @@ select top 50 percent * from Person
 select * from Person order by cast(Age as int)
 select * from Person order by Age
 
---?
+--summeerib vanuse
 select sum(cast(Age as int)) from Person
 
 --- kuvab kõige nooremat isikut
