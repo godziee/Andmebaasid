b8a5137029ded4b19f439bb8e29f64a770eee7b9
Bogdan Jakovlev <bogdan.jakovlev11@gmail.com>
2024-05-08 13:07:58 +0300

    sorteerimine vanuse järgi

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -143,7 +143,7 @@ select top 3 Age, Name from Person
 
 --- näitab esimesed 50% tabelis
 select top 50 percent * from Person
---?
+-- sorteerimine vanuse järgi
 select * from Person order by cast(Age as int)
 select * from Person order by Age
 
