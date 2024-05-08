124e1de40c58b97cf3923f6d63f6b03c35f3a3cf
Bogdan Jakovlev <bogdan.jakovlev11@gmail.com>
2024-05-08 13:12:12 +0300

    sisestame väärtuse tabelisse ja kuvame Department tabelit

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -181,7 +181,7 @@ Salary nvarchar(50),
 DepartmentId int
 )
 
---?
+--sisestame v‰‰rtuse tabelisse ja kuvame Department tabelit 
 insert into Department (Id, DepartmentName, Location, DepartmentHead)
 values (1, 'IT', 'London', 'Rick')
 insert into Department (Id, DepartmentName, Location, DepartmentHead)
